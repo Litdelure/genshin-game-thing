e116570d489c624ea430707da55c5103b76056a2
hebe871 <chuhy055@mymail.unisa.edu.au>
2026-03-05 23:26:33 +1030

    edited what apps the django will use

--- thing/genshin/settings.py
+++ thing/genshin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'enemies.apps.EnemiesConfig',
+    'player.apps.PlayerConfig'
 ]
 
 MIDDLEWARE = [
