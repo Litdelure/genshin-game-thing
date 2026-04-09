f936e0bb771499c3702698ecbb73f10e80a6fb98
hebe871 <chuhy055@mymail.unisa.edu.au>
2026-04-09 12:59:09 +0930

    Added player attributes and images

--- thing/player/models.py
+++ thing/player/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    health=models.IntegerField(default=100)
+    attack=models.IntegerField(default=100)
+    speed=models.IntegerField(default=100)
+    statMultiplier=models.IntegerField(default=1)
+    
+
+
+
